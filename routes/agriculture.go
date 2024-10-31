f8242f69211dd0b42da78750e8fa011047edd712
Daniel Johnsen <daniel.johnsen@knowit.no>
2024-10-31 10:14:25 +0100

    fix: add redoc and swagger

--- routes/agriculture.go
+++ routes/agriculture.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+// @BasePath /api/v1
+
+// Agriculture godoc
+// @Summary Get summary of agriculture data
+// @Schemes
+// @Description Gets aggregated data from Deforestation, Flood, Weather and Soil APIs. See the [API documentation](https://developer.openepi.io) for more information.
+// @Tags summary
+// @Produce json
+// @Param   lat     query    string     true        "Latitude"
+// @Param   lon     query    string     true        "Longitude"
+// @Success 200 {object} models.Summary
+// @Router /summary [get]
 func Agriculture(c *gin.Context) {
 	response := handlers.Summary(c)
 	c.JSON(http.StatusOK, response)
